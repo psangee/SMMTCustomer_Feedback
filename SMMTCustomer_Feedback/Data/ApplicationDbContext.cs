01321cef48af33a42270528b2e3150a6c5e33cf2
psangee <128517166+psangee@users.noreply.github.com>
2024-11-19 15:25:34 +0000

    Add project files.

---
+++ SMMTCustomer_Feedback/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SMMTCustomer_Feedback.Models;
+
+namespace SMMTCustomer_Feedback.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        public DbSet<SMMTCustomer_Feedback.Models.CustFeedback> CustFeedback { get; set; } = default!;
+    }
+}
---
+++ SMMTCustomer_Feedback/Models/CustFeedback.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SMMTCustomer_Feedback.Models
+{
+    public class CustFeedback
+    {
+        [Key]
+        public int FeedbackID { get; set; }
+        [Required]
+        public string CustomerName { get; set; }
+        [Required]
+        public string FeedbackMessage { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime DateSubmitted { get; set; }
+
+    }
+}
